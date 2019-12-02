5f2e40b517cf24e06b61ea924f9286a9481ef57b
calvinhill <andre.escola.2008@gmail.com>
2019-12-02 23:43:59 +0000

    SQL Scripts business

---
+++ FBB/FBB.Business/Scripts.DB/scripts init.sql
@@ -0,0 +1,57 @@
+CREATE SCHEMA `fbb` ;
+
+USE `fbb`;
+
+CREATE TABLE `aspnetusers` (
+  `Id` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `Email` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `EmailConfirmed` tinyint(1) NOT NULL,
+  `PasswordHash` longtext,
+  `SecurityStamp` longtext,
+  `PhoneNumber` longtext,
+  `PhoneNumberConfirmed` tinyint(1) NOT NULL,
+  `TwoFactorEnabled` tinyint(1) NOT NULL,
+  `LockoutEndDateUtc` datetime DEFAULT NULL,
+  `LockoutEnabled` tinyint(1) NOT NULL,
+  `AccessFailedCount` int(11) NOT NULL,
+  `UserName` varchar(256) CHARACTER SET utf8 NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `ImageUrl` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `aspnetuserroles` (
+  `UserId` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `RoleId` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `EntityId` varchar(36) NOT NULL DEFAULT '',
+  PRIMARY KEY (`UserId`,`RoleId`,`EntityId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `aspnetroles` (
+  `Id` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `Name` varchar(256) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `aspnetuserclaims` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `aspnetuserlogins` (
+  `LoginProvider` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `ProviderKey` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `UserId` varchar(128) CHARACTER SET utf8 NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `__migrationhistory` (
+  `MigrationId` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `ContextKey` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `Model` longblob NOT NULL,
+  `ProductVersion` varchar(32) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`MigrationId`,`ContextKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
